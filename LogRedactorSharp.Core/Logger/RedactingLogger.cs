e6d4194d4c848200dff7f106ea65f58d7b335252
Leo <16503599+lkukhar@users.noreply.github.com>
2023-10-21 09:28:15 -0400

    Add Not Implemented Redactor

---
+++ LogRedactorSharp.Core/Logger/RedactingLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LogRedactorSharp.Core.Logger;
+
+public sealed class RedactingLogger : ILogger
+{
+    private readonly ILogger _logger;
+
+    public RedactingLogger(ILogger logger) =>
+        _logger = logger;
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull =>
+        _logger.BeginScope(state)!;
+
+    public bool IsEnabled(LogLevel logLevel) =>
+        _logger.IsEnabled(logLevel);
+
+
+
+    public void Log<TState>(
+        LogLevel logLevel, 
+        EventId eventId, 
+        TState state, 
+        Exception? exception, 
+        Func<TState, Exception?, string> formatter)
+    {
+        throw new NotImplementedException();
+    }
+}

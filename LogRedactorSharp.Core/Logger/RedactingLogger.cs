4513e0870668e2b20fe93f4d5895591480eacac2
Leo <16503599+lkukhar@users.noreply.github.com>
2023-10-28 07:32:04 -0400

    Add BuildNonRedactedProperties Logic

--- LogRedactorSharp.Core/Logger/RedactingLogger.cs
+++ LogRedactorSharp.Core/Logger/RedactingLogger.cs
@@ -1,29 +1,112 @@
 ﻿using Microsoft.Extensions.Logging;
+using LogRedactorSharp.Core.Attributes;
+using System.Reflection;
+using System.Text;
 
 namespace LogRedactorSharp.Core.Logger;
 
-public sealed class RedactingLogger : ILogger
+/// <summary>
+/// Custom logger used to redact sensetive information by keying
+/// on properties with the <see cref="RedactAttribute"/>
+/// </summary>
+internal sealed class RedactingLogger : ILogger
 {
     private readonly ILogger _logger;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="logger"></param>
     public RedactingLogger(ILogger logger) =>
         _logger = logger;
 
+    /// <summary>
+    /// Default begin scope implementation
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <param name="state"></param>
+    /// <returns></returns>
     public IDisposable BeginScope<TState>(TState state) where TState : notnull =>
         _logger.BeginScope(state)!;
 
+    /// <summary>
+    /// Default IsEnabled implementation
+    /// </summary>
+    /// <param name="logLevel"></param>
+    /// <returns></returns>
     public bool IsEnabled(LogLevel logLevel) =>
         _logger.IsEnabled(logLevel);
 
-
-
+    /// <summary>
+    /// Default Implementation of Log
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <param name="logLevel"></param>
+    /// <param name="eventId"></param>
+    /// <param name="state"></param>
+    /// <param name="exception"></param>
+    /// <param name="formatter"></param>
     public void Log<TState>(
-        LogLevel logLevel, 
-        EventId eventId, 
-        TState state, 
-        Exception? exception, 
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
         Func<TState, Exception?, string> formatter)
     {
-        throw new NotImplementedException();
+        if (state is IReadOnlyList<KeyValuePair<string, object?>>)
+        {
+            _logger.Log(
+                logLevel,
+                eventId, 
+                state, 
+                exception,
+                (o, e) => PrintNonRedactedProperties(o));
+        }
+        else
+        {
+            _logger.Log(logLevel, eventId, state, exception, formatter);
+        }
+    }
+
+    private string PrintNonRedactedProperties(object? obj)
+    {
+        var stringBuilder = new StringBuilder();
+        return BuildNonRedactedProperties(stringBuilder, obj, 0).ToString();
+    }
+
+    private StringBuilder BuildNonRedactedProperties(StringBuilder stringBuilder, object? obj, int indent)
+    {
+        if(obj == null)
+        {
+            return stringBuilder;
+        }
+
+        var indentString = new string(' ', indent);
+        var objType = obj.GetType();
+        var nonRedeactedProperties = objType.GetProperties()
+            .Where(e => e.GetCustomAttribute<RedactAttribute>(true) == null)
+            .ToList();
+
+        foreach (var property in nonRedeactedProperties)
+        {
+            stringBuilder.Append(Environment.NewLine);
+
+            var propertyValue = property?.GetValue(obj, null);
+            if (Convert.GetTypeCode(propertyValue) == TypeCode.Object)
+            {
+                stringBuilder.Append(Environment.NewLine);
+                stringBuilder.Append($"{indentString}{property?.Name}:");
+                stringBuilder.Append(Environment.NewLine);
+                stringBuilder.Append(indentString + "{");
+                BuildNonRedactedProperties(stringBuilder, propertyValue, indent + 2);
+                stringBuilder.Append(indentString + "}");
+            }
+            else
+            {
+                stringBuilder.Append($"{indentString}{property?.Name}: {propertyValue}");
+            }
+        }
+
+        return stringBuilder;
     }
 }
---
+++ LogRedactorSharp.Core/Logger/RedactingLoggerProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LogRedactorSharp.Core.Logger;
+
+/// <summary>
+/// Redacting Logger Provider
+/// </summary>
+public sealed class RedactingLoggerProvider : ILoggerProvider
+{
+    private readonly ILoggerProvider _loggerProvider;
+
+    /// <summary>
+    /// Default Constructor
+    /// </summary>
+    /// <param name="loggerProvider"></param>
+    public RedactingLoggerProvider(ILoggerProvider loggerProvider) =>
+        _loggerProvider = loggerProvider;
+
+    /// <summary>
+    /// Default dispose
+    /// </summary>
+    public void Dispose() => _loggerProvider.Dispose();
+
+    /// <summary>
+    /// Create Logger
+    /// </summary>
+    /// <param name="categoryName"></param>
+    /// <returns></returns>
+    public ILogger CreateLogger(string categoryName)
+    {
+        var logger = _loggerProvider.CreateLogger(categoryName);
+        return new RedactingLogger(logger);
+    }
+}
